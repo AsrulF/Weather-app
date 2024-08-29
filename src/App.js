e15a65c22b867098da3509fa440d56d0ece63e88
AsrulF <asrulfirdaus93@gmail.com>
2024-08-29 10:57:32 +0800

    Finished Project Weather App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import SearchInput from "./components/search-input";
+import WeatherInfo from "./components/weather-info";
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-[#222831] w-screen h-screen text-[#EEEEEE] flex flex-col items-center justify-center">
+      <div className="bg-[#31363F] p-10 rounded-lg flex flex-col items-center justify-center">
+        <SearchInput />
+      </div>
     </div>
   );
 }
